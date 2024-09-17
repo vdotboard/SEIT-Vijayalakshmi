1da34abded524727ea4efd3d458a9aa8b4d1f380
vdotboard <42177694+vdotboard@users.noreply.github.com>
2024-09-17 15:51:26 +0100

    Update SortableTable.java
    removed unwanted comments

--- maven/src/test/java/io/cucumber/skeleton/glue/SortableTable.java
+++ maven/src/test/java/io/cucumber/skeleton/glue/SortableTable.java
@@ -30,24 +30,24 @@ public class SortableTable extends Context {
 
     @Then("I should see the following rows in the example1 table:")
     public void i_should_see_the_following_rows_in_the_example1_table(io.cucumber.datatable.DataTable expectedTable) {
-        // Find the table
+       
         WebElement table = driver.findElement(By.id("table1"));
 
-        // Find all rows in the table body
+       
         List<WebElement> rows = table.findElements(By.xpath(".//tbody/tr"));
 
-        // Convert the expected data from Cucumber DataTable to a list of maps
+      
         List<Map<String, String>> expectedRows = expectedTable.asMaps(String.class, String.class);
 
-        // Loops through each row and verify its content
+       
         for (int i = 0; i < expectedRows.size(); i++) {
             // Find each cell in the current row
             List<WebElement> cells = rows.get(i).findElements(By.tagName("td"));
 
-            // Retrieve the expected values for the current row
+    
             Map<String, String> expectedRow = expectedRows.get(i);
 
-            // Assert the values of each cell
+      
             assertEquals(expectedRow.get("Last Name"), cells.get(0).getText());
            assertEquals( expectedRow.get("First Name"), cells.get(1).getText());
          assertEquals(expectedRow.get("Email"), cells.get(2).getText());
